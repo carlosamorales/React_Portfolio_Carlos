8cb5565a99e38cc5494e49d28ad5887ed739d99a
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-06-06 22:36:50 -0500

    Adding code for index.html, App.jsx and main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Router>
+    <Header />
+    <main>
+      <Switch>
+        <Route exact path="/" component={AboutMe} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
+    </main>
+    <Footer />
+  </Router>
+);
+
+export default App;
