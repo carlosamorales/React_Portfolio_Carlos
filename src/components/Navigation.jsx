d7af297a2e29e4076d5b9ea9291faf3a337a72b3
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-06-08 21:24:59 -0500

    Updating Navigation.jsx and styles.css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,10 +4,26 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => (
   <nav>
     <ul>
-      <li><NavLink exact to="/" activeClassName="active">About Me</NavLink></li>
-      <li><NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink></li>
-      <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
-      <li><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
+      <li>
+        <NavLink exact to="/" activeClassName="active">
+          About Me
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/portfolio" activeClassName="active">
+          Portfolio
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact" activeClassName="active">
+          Contact
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/resume" activeClassName="active">
+          Resume
+        </NavLink>
+      </li>
     </ul>
   </nav>
 );
