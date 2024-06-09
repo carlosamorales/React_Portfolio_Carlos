c3d27b9749fd950743a5fb105f06201312b062c1
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-06-08 21:41:46 -0500

    moving the profile image into the public folder in order to deploy into netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Navigation from './Navigation';
-import profilePicture from '../assets/images/projects/carlos-images.jpg'; // Adjust the path accordingly
 
 const Header = () => (
   <header>
     <div className="header-content">
-      <img src={profilePicture} alt="Carlos Morales" className="profile-picture" />
+      <img src="/carlos-images.jpg" alt="Carlos Morales" className="profile-picture" />
       <h1>Carlos's Portfolio</h1>
     </div>
     <Navigation />
